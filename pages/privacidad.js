f5342ade1c965f3821da57d713358b562ad32558
Hardash01 <edwin98santos@gmail.com>
2023-10-12 18:32:53 -0600

    pagina aviso de privacidad

--- pages/privacidad.js
+++ pages/privacidad.js
@@ -5,9 +5,26 @@ export default function Privacidad() {
         <Layout
             title={'Aviso de Privacidad'}
         >
-            <h1 className="text-6xl font-extrabold uppercase text-center">
-                Aviso de Privacidad
-            </h1>
+            <div className="md:w-5/6 lg:w-5/6 mx-auto max-w-screen-xl py-16 px-3 lg:px-0">
+                <h2 className="uppercase text-3xl font-medium text-center md:text-left mb-10">Aviso de privacidad | Bienes Raices</h2>
+                <div className="space-y-5">
+                    <p className="uppercase">SE EMITE EL PRESENTE AVISO DE PRIVACIDAD DE LA EMPRESA BIENES RAICES, S. DE R.L. DE C.V., EN BASE A LO ESTABLECIDO EN LA LEY:</p>
+                    <p className="uppercase">Nullam enim metus, molestie eget venenatis eu, laoreet sed lectus.</p>
+                    <p>In ut volutpat orci, quis mollis nunc. Morbi vel nunc vehicula, dictum orci ac, vehicula tellus. Nulla facilisi. Sed vestibulum ultrices ex, a consequat eros sodales nec. Maecenas sollicitudin faucibus ornare. Etiam luctus, sapien quis malesuada dictum, turpis ex posuere leo, et ullamcorper ipsum erat eget turpis. Aliquam sit amet ligula tempor, commodo mi ultrices, aliquet ipsum. Nulla volutpat, augue et condimentum pharetra, dui turpis lacinia dui, vitae molestie magna nisl at magna. Donec nunc nibh, maximus ac varius vel, malesuada et ex. Aliquam efficitur aliquet tortor eget cursus. Integer aliquam odio sit amet viverra ultricies. Etiam non lectus quis lectus porttitor ultrices.</p>
+                    <p className="uppercase">Nullam enim metus, molestie eget venenatis eu, laoreet sed lectus.</p>
+                    <p>In ut volutpat orci, quis mollis nunc. Morbi vel nunc vehicula, dictum orci ac, vehicula tellus. Nulla facilisi. Sed vestibulum ultrices ex, a consequat eros sodales nec. Maecenas sollicitudin faucibus ornare. Etiam luctus, sapien quis malesuada dictum, turpis ex posuere leo, et ullamcorper ipsum erat eget turpis. Aliquam sit amet ligula tempor, commodo mi ultrices, aliquet ipsum. Nulla volutpat, augue et condimentum pharetra, dui turpis lacinia dui, vitae molestie magna nisl at magna. Donec nunc nibh, maximus ac varius vel, malesuada et ex. Aliquam efficitur aliquet tortor eget cursus. Integer aliquam odio sit amet viverra ultricies. Etiam non lectus quis lectus porttitor ultrices.</p>
+
+                    <p className="uppercase">Nullam enim metus, molestie eget venenatis eu, laoreet sed lectus.</p>
+                    <p>In ut volutpat orci, quis mollis nunc. Morbi vel nunc vehicula, dictum orci ac, vehicula tellus. Nulla facilisi. Sed vestibulum ultrices ex, a consequat eros sodales nec. Maecenas sollicitudin faucibus ornare. Etiam luctus, sapien quis malesuada dictum, turpis ex posuere leo, et ullamcorper ipsum erat eget turpis. Aliquam sit amet ligula tempor, commodo mi ultrices, aliquet ipsum. Nulla volutpat, augue et condimentum pharetra, dui turpis lacinia dui, vitae molestie magna nisl at magna. Donec nunc nibh, maximus ac varius vel, malesuada et ex. Aliquam efficitur aliquet tortor eget cursus. Integer aliquam odio sit amet viverra ultricies. Etiam non lectus quis lectus porttitor ultrices.</p>
+
+                    <p className="uppercase">Nullam enim metus, molestie eget venenatis eu, laoreet sed lectus.</p>
+                    <p>In ut volutpat orci, quis mollis nunc. Morbi vel nunc vehicula, dictum orci ac, vehicula tellus. Nulla facilisi. Sed vestibulum ultrices ex, a consequat eros sodales nec. Maecenas sollicitudin faucibus ornare. Etiam luctus, sapien quis malesuada dictum, turpis ex posuere leo, et ullamcorper ipsum erat eget turpis. Aliquam sit amet ligula tempor, commodo mi ultrices, aliquet ipsum. Nulla volutpat, augue et condimentum pharetra, dui turpis lacinia dui, vitae molestie magna nisl at magna. Donec nunc nibh, maximus ac varius vel, malesuada et ex. Aliquam efficitur aliquet tortor eget cursus. Integer aliquam odio sit amet viverra ultricies. Etiam non lectus quis lectus porttitor ultrices.</p>
+
+                    <p className="uppercase">Nullam enim metus, molestie eget venenatis eu, laoreet sed lectus.</p>
+                    <p>In ut volutpat orci, quis mollis nunc. Morbi vel nunc vehicula, dictum orci ac, vehicula tellus. Nulla facilisi. Sed vestibulum ultrices ex, a consequat eros sodales nec. Maecenas sollicitudin faucibus ornare. Etiam luctus, sapien quis malesuada dictum, turpis ex posuere leo, et ullamcorper ipsum erat eget turpis. Aliquam sit amet ligula tempor, commodo mi ultrices, aliquet ipsum. Nulla volutpat, augue et condimentum pharetra, dui turpis lacinia dui, vitae molestie magna nisl at magna. Donec nunc nibh, maximus ac varius vel, malesuada et ex. Aliquam efficitur aliquet tortor eget cursus. Integer aliquam odio sit amet viverra ultricies. Etiam non lectus quis lectus porttitor ultrices.</p>
+
+                </div>
+            </div>
         </Layout>
     )
 }
